5a5e2e91a36f09e06a2e10e74b229d3aead6f148
fuyura-dev <147980387+fuyura-dev@users.noreply.github.com>
2025-12-06 17:09:44 +0800

    pythonBridge

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const { runPython } = require("./utils/pythonBridge");
+
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/test", async (req, res) => {
+  try {
+    const result = await runPython("external/test/test.py", ["adfasf"]);
+
+    res.json({ output: result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const PORT = 3000;
