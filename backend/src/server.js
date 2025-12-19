0421408f3a8b85ea06b8894d9102e9a81f0b888c
fuyura-dev <147980387+fuyura-dev@users.noreply.github.com>
2025-12-19 21:14:02 +0800

    add root words API endpoint and JSON data file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,48 @@
 const { runPython } = require("./utils/pythonBridge");
 
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
+app.use(express.json());
+
+const rootWordsPath = path.join(__dirname, "data", "root_words.json");
+
+app.get("/api/roots", (req, res) => {
+  try {
+    let data = fs.readFileSync(rootWordsPath, "utf8");
+
+    if (data.charCodeAt(0) === 0xfeff) {
+      data = data.slice(1);
+    }
+
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to read file", details: err.message });
+  }
+});
+
+app.post("/api/analyze", (req, res) => {
+  const { word } = req.body;
+
+  if (!word) {
+    return res.status(400).json({ error: "error input" });
+  }
+
+  res.json({
+    input: word,
+    root: "root test",
+    affixes: ["affix 1", "affix 2"],
+    valid: true,
+  });
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello Hello");
 });
 
 app.get("/test", async (req, res) => {
