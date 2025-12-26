c7a53eed3c05837f7d0c4886de520beafd7528f1
fuyura-dev <147980387+fuyura-dev@users.noreply.github.com>
2025-12-27 00:43:45 +0800

    Set PORT to use environment variable or default to 3000

--- backend/src/server.js
+++ backend/src/server.js
@@ -71,7 +71,7 @@ app.get("/", (req, res) => {
   res.send("Hello Hello");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
