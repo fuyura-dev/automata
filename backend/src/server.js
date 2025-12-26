d1026aa589011c428aaedc8d16582c28d9848dbd
fuyura-dev <147980387+fuyura-dev@users.noreply.github.com>
2025-12-27 01:34:50 +0800

    Add CORS support and update package dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,14 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const cors = require("cors");
 const app = express();
+
 app.use(express.json());
+
+app.use(cors({
+  origin: '*'
+}))
 
 const rootWordsPath = path.join(
   __dirname,
